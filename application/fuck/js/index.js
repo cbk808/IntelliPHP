dffab34ea92e3d099f76fdec89c54c98469edeba
root <root@localhost.localdomain>
2015-05-24 18:00:01 -0400

    auto commited

--- application/fuck/js/index.js
+++ application/fuck/js/index.js
@@ -1,7 +1,4 @@
 /**
  * Created by midix on 2015/5/24.
  */
-
-
-document.getElementById("biaoti").style.color="yellow";
-document.getElementById("biaoti").innerHTML="sdfwweffsffffwdf";
+document.getElementById("qqq").innerHTML="2粉23232";
