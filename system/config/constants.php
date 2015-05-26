0beeadbdaaaa2095fdc5834b2321e0a04a5e4a87
root <root@localhost.localdomain>
2015-05-26 10:00:01 -0400

    auto commited

--- system/config/constants.php
+++ system/config/constants.php
@@ -1,2 +1,3 @@
 <?php
 define(CORE_DIR,BASE_DIR."/system/core");
+define(LIB_DIR,BASE_DIR."/system/library");
--- system/shell.php
+++ system/shell.php
@@ -1,7 +1,12 @@
 <?php
-function autoLoader()
+function autoLoader($class)
 {
-
+    if(file_exists(CORE_DIR."/".$class)){
+        include_once CORE_DIR."/".$class;
+    }elseif(file_exists(LIB_DIR."/".$class)){
+        include_once LIB_DIR."/".$class;
+    }
 }
 spl_autoload_register(autoLoader);
+
 
