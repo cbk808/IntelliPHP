e0286d50a76bb3ce5b4b985ac695c57ab7a9b0f9
root <root@localhost.localdomain>
2015-05-26 18:00:01 -0400

    auto commited

---
+++ application/portal/php/config/PortalIndex.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: midix
+ * Date: 2015/5/26
+ * Time: 21:50
+ */
+
+class PortalIndex {
+    public function __construct(){
+                
+    }
+}
--- system/core/AppBase.php
+++ system/core/AppBase.php
@@ -9,11 +9,12 @@
 class AppBase {
     protected $url_params;//url参数数组
     protected $count_url_params;//url参数个数
-    protected 
-    protected
+    protected $module;//入口模块
+    protected $entry;//入口函数
+    protected $app_conf;//app的配置数组
     public function __construct(){
-        global $url_snippet,$count_usl_snippet;;
-        $this->count_url_params=$count_usl_snippet;
+        global $url_snippet,$count_url_snippet;
+        $this->count_url_params=$count_url_snippet;
         $this->url_params=$url_snippet;
     }
     protected function init(){
