3e1995da4d133069fe8451f75d55e0cecda288d9
root <root@localhost.localdomain>
2015-05-26 14:00:01 -0400

    auto commited

--- system/core/Router.php
+++ system/core/Router.php
@@ -2,6 +2,5 @@
 class Router
 {
     static function start(){
-        
     }
 }
--- system/shell.php
+++ system/shell.php
@@ -2,16 +2,18 @@
 
 include BASE_DIR."/system/config/constants.php";
 //注册自动装载函数
+
 function autoLoader($class)
 {
-    if(file_exists(CORE_DIR."/".$class)){
-        include_once CORE_DIR."/".$class;
-    }elseif(file_exists(LIB_DIR."/".$class)){
-        include_once LIB_DIR."/".$class;
+    if(file_exists(CORE_DIR."/".$class.".php")){
+        include_once CORE_DIR."/".$class.".php";
+    }elseif(file_exists(LIB_DIR."/".$class.".php")){
+        include_once LIB_DIR."/".$class.".php";
     }
 }
+
 //启用自动装载
-spl_autoload_register(autoLoader);
+spl_autoload_register("autoLoader");
 Configure::$conf_files=include_once(CONF_DIR."/conf_files.php");
 
 //初始化输入
